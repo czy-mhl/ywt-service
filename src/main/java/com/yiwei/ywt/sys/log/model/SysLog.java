7eb18afa1b4128ce4946049ae8b6f7c9cf3eb4f0
cai_ziyong@163.com <cyx06050329>
2019-04-15 14:20:52 +0800

    日志记录注解开发

--- src/main/java/com/yiwei/ywt/sys/log/model/SysLog.java
+++ src/main/java/com/yiwei/ywt/sys/log/model/SysLog.java
@@ -3,6 +3,7 @@ package com.yiwei.ywt.sys.log.model;
 
 import com.yiwei.ywt.framework.model.BaseEntity;
 import com.yiwei.ywt.sys.user.model.SysUser;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -13,6 +14,7 @@ import java.util.Date;
  * annotation:SysLogService 实体类
  * table:   T_SYS_LOG
  */
+@Data
 public class SysLog extends BaseEntity implements Serializable {
     /**
      *访问地址
@@ -34,8 +36,14 @@ public class SysLog extends BaseEntity implements Serializable {
      *操作类型(操作类型1=新增，2=修改，3=查询，4=删除，5=审核，6=发布 , 7=撤销 ,8=处理,9=其他)
      * */
     private String operateType;
-    private String operationDesc; //操作类型描述
-    private String menuName; //菜单名称
+    /**
+     * 操作类型描述
+     */
+    private String operationDesc;
+    /**
+     * 菜单名称
+     */
+    private String menuName;
     /**
      * 访问模块中文名称
      * */
@@ -85,139 +93,5 @@ public class SysLog extends BaseEntity implements Serializable {
      */
     private SysUser sysUser;
 
-    public SysUser getSysUser() {
-        return sysUser;
-    }
 
-    public void setSysUser(SysUser sysUser) {
-        this.sysUser = sysUser;
-    }
-
-    public String getModuleContent() { return moduleContent; }
-
-    public void setModuleContent(String moduleContent) { this.moduleContent = moduleContent; }
-
-    public String getOperationDesc() { return operationDesc; }
-
-    public void setOperationDesc(String operationDesc) { this.operationDesc = operationDesc; }
-
-    public String getMenuName() { return menuName; }
-
-    public void setMenuName(String menuName) { this.menuName = menuName; }
-
-    public String getRequestUri() {
-        return requestUri;
-    }
-
-    public void setRequestUri(String requestUri) {
-        this.requestUri = requestUri;
-    }
-
-    public String getRequestIp() {
-        return requestIp;
-    }
-
-    public void setRequestIp(String requestIp) {
-        this.requestIp = requestIp;
-    }
-
-    public Long getSysUserId() {
-        return sysUserId;
-    }
-
-    public void setSysUserId(Long sysUserId) {
-        this.sysUserId = sysUserId;
-    }
-
-    public String getSysUserAccount() {
-        return sysUserAccount;
-    }
-
-    public void setSysUserAccount(String sysUserAccount) {
-        this.sysUserAccount = sysUserAccount;
-    }
-
-    public String getOperateType() { return operateType; }
-
-    public void setOperateType(String operateType) { this.operateType = operateType; }
-
-    public String getModuleName() {
-        return moduleName;
-    }
-
-    public void setModuleName(String moduleName) {
-        this.moduleName = moduleName;
-    }
-
-    public String getModuleCode() {
-        return moduleCode;
-    }
-
-    public void setModuleCode(String moduleCode) {
-        this.moduleCode = moduleCode;
-    }
-
-    public String getMethodName() {
-        return methodName;
-    }
-
-    public void setMethodName(String methodName) {
-        this.methodName = methodName;
-    }
-
-    public String getMethodCode() {
-        return methodCode;
-    }
-
-    public void setMethodCode(String methodCode) {
-        this.methodCode = methodCode;
-    }
-
-    public Integer getOperateResult() {
-        return operateResult;
-    }
-
-    public void setOperateResult(Integer operateResult) {
-        this.operateResult = operateResult;
-    }
-
-    public String getLogContent() {
-        return logContent;
-    }
-
-    public void setLogContent(String logContent) {
-        this.logContent = logContent;
-    }
-
-    public Date getOperateTime() {
-        return operateTime;
-    }
-
-    public void setOperateTime(Date operateTime) {
-        this.operateTime = operateTime;
-    }
-
-    public Date getOperateTimeBefore() {
-        return operateTimeBefore;
-    }
-
-    public void setOperateTimeBefore(Date operateTimeBefore) {
-        this.operateTimeBefore = operateTimeBefore;
-    }
-
-    public Date getOperateTimeAfter() {
-        return operateTimeAfter;
-    }
-
-    public void setOperateTimeAfter(Date operateTimeAfter) {
-        this.operateTimeAfter = operateTimeAfter;
-    }
-
-    public Integer getDelFlag() {
-        return delFlag;
-    }
-
-    public void setDelFlag(Integer delFlag) {
-        this.delFlag = delFlag;
-    }
 }
