459db106411cca6b9f14d01761d96e35f846dce8
MyXk95 <2743433326@qq.com>
2019-04-18 09:35:26 +0800

    验表申请

--- src/main/java/com/yiwei/ywt/businessProcess/checkWaterMeter/model/CheckWaterMeter.java
+++ src/main/java/com/yiwei/ywt/businessProcess/checkWaterMeter/model/CheckWaterMeter.java
@@ -6,14 +6,24 @@ import lombok.Data;
 
 @Data
 public class CheckWaterMeter extends BaseEntity {
-
+    /**
+     * 户号
+     */
     private String familyNumber;
-
+    /**
+     * 验表原因
+     */
     private String checkWaterMeterReason;
-
+    /**
+     * 办理人
+     */
     private String transactor;
-
+    /**
+     * 办理人手机
+     */
     private String transactorMobile;
-
+    /**
+     * 户主身份证
+     */
     private String idCardImage;
 }
